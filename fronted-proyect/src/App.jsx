5b7a075d0e32002d46a8cfcce19e9168a8f984f0
Yoan Sebastian <yoansebastianramirezgarcia@gmail.com>
2025-02-02 18:50:04 -0500

    esta es la version 1.1 de mi calculadora

---
+++ fronted-proyect/src/App.jsx
@@ -0,0 +1,33 @@
+const App = () => {
+  return (
+    <div>
+      <div className="box">
+        <label className="input">
+          <input className="btn-input" type="text" />
+        </label>
+        <div className="btn btn-red">AC</div>
+        <div className="btn">%</div>
+        <div className="btn">X</div>
+        <div className="btn">÷</div>
+        <div className="btn">7</div>
+        <div className="btn">8</div>
+        <div className="btn">9</div>
+        <div className="btn">X</div>
+        <div className="btn">4</div>
+        <div className="btn">5</div>
+        <div className="btn">6</div>
+        <div className="btn">-</div>
+        <div className="btn">1</div>
+        <div className="btn">2</div>
+        <div className="btn">3</div>
+        <div className="btn">+</div>
+        <div className="btn">+/-</div>
+        <div className="btn">0</div>
+        <div className="btn">.</div>
+        <div className="btn btn-yellow">=</div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
