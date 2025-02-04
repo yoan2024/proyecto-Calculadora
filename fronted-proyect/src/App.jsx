68624333c3c60acadc8707b170f515e89a8adeff
Yoan Sebastian <yoansebastianramirezgarcia@gmail.com>
2025-02-04 14:00:37 -0500

    Añadiendo funcionalidad ala calculadora, version 3.0

--- fronted-proyect/src/App.jsx
+++ fronted-proyect/src/App.jsx
@@ -1,29 +1,73 @@
+import { useReducer, useState } from "react";
 const App = () => {
+  const [value, setInput] = useState("");
+
   return (
     <div className="box1">
       <label className="input">
-        <input className="btn-input rtl-input" type="text" />
+        <input className="btn-input rtl-input" type="text" value={value} />
       </label>
       <div className="box2">
-        <div className="btn btn-red">AC</div>
-        <div className="btn">%</div>
-        <div className="btn">X</div>
-        <div className="btn">÷</div>
-        <div className="btn">7</div>
-        <div className="btn">8</div>
-        <div className="btn">9</div>
-        <div className="btn">-</div>
-        <div className="btn">4</div>
-        <div className="btn">5</div>
-        <div className="btn">6</div>
-        <div className="btn">+</div>
-        <div className="btn">1</div>
-        <div className="btn">2</div>
-        <div className="btn">3</div>
-        <div className="btn">+/-</div>
-        <div className="btn">0</div>
-        <div className="btn">.</div>
-        <div className="btn btn-yellow">=</div>
+        <div className="btn btn-red" onClick={() => setInput("")}>
+          AC
+        </div>
+        <div className="btn" onClick={(e) => setInput(value + "%")}>
+          %
+        </div>
+        <div className="btn" onClick={(e) => setInput(value + "*")}>
+          *
+        </div>
+        <div className="btn" onClick={(e) => setInput(value + "÷")}>
+          ÷
+        </div>
+        <div className="btn" onClick={(e) => setInput(value + "7")}>
+          7
+        </div>
+        <div className="btn" onClick={(e) => setInput(value + "8")}>
+          8
+        </div>
+        <div className="btn" onClick={(e) => setInput(value + "9")}>
+          9
+        </div>
+        <div className="btn" onClick={(e) => setInput(value + "-")}>
+          -
+        </div>
+        <div className="btn" onClick={(e) => setInput(value + "4")}>
+          4
+        </div>
+        <div className="btn" onClick={(e) => setInput(value + "5")}>
+          5
+        </div>
+        <div className="btn" onClick={(e) => setInput(value + "6")}>
+          6
+        </div>
+        <div className="btn" onClick={(e) => setInput(value + "+")}>
+          +
+        </div>
+        <div className="btn" onClick={(e) => setInput(value + "1")}>
+          1
+        </div>
+        <div className="btn" onClick={(e) => setInput(value + "2")}>
+          2
+        </div>
+        <div className="btn" onClick={(e) => setInput(value + "3")}>
+          3
+        </div>
+        <div
+          className="btn"
+          onClick={(e) => setInput({ value: "0" } + "Esto esta desabilitado")}
+        >
+          +/-
+        </div>
+        <div className="btn" onClick={(e) => setInput(value + "0")}>
+          0
+        </div>
+        <div className="btn" onClick={(e) => setInput(value + ".")}>
+          .
+        </div>
+        <div className="btn btn-yellow" onClick={(e) => setInput(eval(value))}>
+          =
+        </div>
       </div>
     </div>
   );
--- fronted-proyect/src/main.jsx
+++ fronted-proyect/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./style/box.css";
 import App from "./App.jsx";
+//import Hola from "./modulo/Hola";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
