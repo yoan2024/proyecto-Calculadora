920a36f78ae5b7fe1e15b42182dffe96b0334651
Yoan Sebastian <yoansebastianramirezgarcia@gmail.com>
2025-02-03 21:03:36 -0500

    Mejorando la interfaz de usuario,  propiedades mas lujosas.Version 2.0

--- fronted-proyect/src/App.jsx
+++ fronted-proyect/src/App.jsx
@@ -1,10 +1,10 @@
 const App = () => {
   return (
-    <div>
-      <div className="box">
-        <label className="input">
-          <input className="btn-input" type="text" />
-        </label>
+    <div className="box1">
+      <label className="input">
+        <input className="btn-input rtl-input" type="text" />
+      </label>
+      <div className="box2">
         <div className="btn btn-red">AC</div>
         <div className="btn">%</div>
         <div className="btn">X</div>
@@ -12,15 +12,14 @@ const App = () => {
         <div className="btn">7</div>
         <div className="btn">8</div>
         <div className="btn">9</div>
-        <div className="btn">X</div>
+        <div className="btn">-</div>
         <div className="btn">4</div>
         <div className="btn">5</div>
         <div className="btn">6</div>
-        <div className="btn">-</div>
+        <div className="btn">+</div>
         <div className="btn">1</div>
         <div className="btn">2</div>
         <div className="btn">3</div>
-        <div className="btn">+</div>
         <div className="btn">+/-</div>
         <div className="btn">0</div>
         <div className="btn">.</div>
